5b5d85e092b3b11b073dbfebfd06bdfd01bf817c
De Maeseneer Didier <didier.demaeseneer@gmail.com>
2021-04-08 13:41:22 +0200

    Update Start-RunElevated.ps1

--- Start-RunElevated.ps1
+++ Start-RunElevated.ps1
@@ -1,10 +1,10 @@
 If ( -not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) )
 {
-    # Relaunch as an elevated process:
+    # Launch elevated process
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
     # Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File",('"{0}"' -f $PSScriptRoot) -Verb RunAs
     exit
 }
 
-# Now running elevated so launch the script:
+# Call script with elevated prompt
 & "$PSScriptRoot\Start-Cleaning.ps1"
